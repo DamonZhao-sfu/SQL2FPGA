9732656f7de0f9b5887cd8c5e090fbad7a1288e5
hza215 <zhao_haikai@sfu.ca>
2024-03-23 06:51:59 -0700

    20240323 native engine support

--- native-engine/NativeORCReader.cpp
+++ native-engine/NativeORCReader.cpp
@@ -1,8 +1,6 @@
 #include "NativeORCReader.h"
-#include "arrow/api.h"
-#include <arrow/dataset/api.h>
-#include <arrow/filesystem/api.h>
-
+#include <arrow/io/api.h>
+#include <jni.h>
 
 NativeORCReader::NativeORCReader(const std::string &file_name) {
 
@@ -27,7 +25,7 @@ std::shared_ptr<arrow::RecordBatch> NativeORCReader::ReadNext() {
     return std::make_shared<arrow::RecordBatch>(batch);
 }
 
-JNIEXPORT jlong JNICALL Java_ca_tudelft_ewi_abs_nonnenmacher_NativeParquetReader_initNativeParquetReader
+JNIEXPORT jlong JNICALL Java_ca_sfu_hiaccel_NativeORCReader_initNativeORCReader
         (JNIEnv *env, jobject, jobject jmemorypool, jstring java_file_name, jbyteArray schema_file_jarr,
          jbyteArray schema_out_jarr,
          jint num_rows) {
@@ -43,23 +41,17 @@ JNIEXPORT jlong JNICALL Java_ca_tudelft_ewi_abs_nonnenmacher_NativeParquetReader
     std::shared_ptr<arrow::Schema> schema_out = ReadSchemaFromProtobufBytes(schema_out_bytes, schema_out_len);
 
     std::shared_ptr<arrow::MemoryPool> pool = std::make_shared<JavaMemoryPool>(env, jmemorypool);
-    return (jlong) new DataSetParquetReader(pool, file_name, schema_file, schema_out, (int) num_rows);
+    return (jlong) new NativeORCReader(pool, file_name, schema_file, schema_out, (int) num_rows);
 }
 
-JNIEXPORT jboolean JNICALL Java_nl_tudelft_ewi_abs_nonnenmacher_NativeParquetReader_readNext
+JNIEXPORT jboolean JNICALL Java_ca_sfu_hiaccel_NativeORCReader_readNext
         (JNIEnv *env, jobject, jlong process_ptr, jlongArray jarr_vector_lengths, jlongArray jarr_vector_null_counts,
          jlongArray jarr_buf_addrs) {
 
-    DataSetParquetReader *datasetParquetReader = (DataSetParquetReader *) process_ptr;
+    NativeORCReader *nativeORCReader = (NativeORCReader *) process_ptr;
 
-    // Read next record batch from parquet file
-    std::shared_ptr<arrow::RecordBatch> out_batch = datasetParquetReader->ReadNext();
-
-    // check if end reached
-    if (recordBatchEnd == out_batch) {
-        return false;
-    }
-
+    std::shared_ptr<arrow::RecordBatch> out_batch = nativeORCReader->ReadNext();
+    
     // Read buffers, field vector length & nullcount from RecordBatch
     const std::shared_ptr<arrow::Schema> &schema = out_batch->schema();
     auto num_fields = schema->num_fields();
@@ -102,33 +94,7 @@ JNIEXPORT jboolean JNICALL Java_nl_tudelft_ewi_abs_nonnenmacher_NativeParquetRea
     return true;
 }
 
-JNIEXPORT void JNICALL Java_nl_tudelft_ewi_abs_nonnenmacher_NativeParquetReader_close
+JNIEXPORT void JNICALL Java_ca_sfu_hiaccel_NativeORCReader_close
         (JNIEnv *env, jobject, jlong process_ptr) {
-    delete (DataSetParquetReader *) process_ptr;
-}
-
-JNIEXPORT void JNICALL Java_nl_tudelft_ewi_abs_nonnenmacher_ParquetReaderEvaluator_readWholeFileWithDefaultMemoryPool
-        (JNIEnv * env, jobject, jstring java_file_name, jbyteArray schema_file_jarr, jint num_rows){
-
-    std::string file_name = get_java_string(env, java_file_name);
-
-    jsize schema_file_len = env->GetArrayLength(schema_file_jarr);
-    jbyte *schema_file_bytes = env->GetByteArrayElements(schema_file_jarr, 0);
-    std::shared_ptr<arrow::Schema> schema_file = ReadSchemaFromProtobufBytes(schema_file_bytes, schema_file_len);
-
-    DataSetParquetReader reader(arrow::MemoryPool::CreateDefault(), file_name, schema_file, schema_file, (int) num_rows);
-
-    auto start_time = std::chrono::steady_clock::now();
-
-    std::shared_ptr<arrow::RecordBatch> out_batch;
-
-    do{
-        out_batch = reader.ReadNext();
-        // check if end reached
-    }while( recordBatchEnd != out_batch);
-
-    auto end_time = std::chrono::steady_clock::now();
-    auto d_milli = std::chrono::duration_cast<std::chrono::milliseconds>( end_time - start_time ).count();
-
-    std::cout << "Duration (CPP):" << d_milli << std::endl;
+    delete (NativeORCReader *) process_ptr;
 }
--- native-engine/NativeORCReader.h
+++ native-engine/NativeORCReader.h
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <arrow/api.h>
 #include <arrow/adapters/orc/adapter.h>
-#include <arrow/dataset/api.h>
 #include <arrow/util/iterator.h>
 
 class NativeORCReader {
 private:
-    std::shared_ptr<arrow::dataset::Dataset> dataset;
-    std::shared_ptr<arrow::dataset::ScanTaskIterator> scan_task_it;
     std::unique_ptr<arrow::adapters::orc::ORCFileReader> fileReader_;
     std::shared_ptr<arrow::RecordBatchReader> recordBatchReader_;
     std::shared_ptr<arrow::RecordBatchIterator> recordBatchIter;
