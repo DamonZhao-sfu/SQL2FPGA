61c025d64cfcbd1a33aaa1163f8e0088407bcbbc
DamonZhao-sfu <hza215@sfu.ca>
2024-05-19 13:53:09 -0700

    test input

--- src/main/scala/sfu/ca/hiaccel/SQL2FPGA_Top.scala
+++ src/main/scala/sfu/ca/hiaccel/SQL2FPGA_Top.scala
@@ -31,9 +31,9 @@ object SQL2FPGA_Top {
 
   // SQL2FPGA_QConfig
   var qConfig = new SQL2FPGA_QConfig
-  val INPUT_DIR_TPCH = "/localhdd/hza215/spark_benchmark/tpch/orc"
+  val INPUT_DIR_TPCH = "/mnt/glusterfs/users/hza214/orc_1"
   val OUTPUT_DIR_TPCH = "/localhdd/hza215/spark_benchmark/tpch/"
-  val INPUT_DIR_TPCDS = "/localhdd/hza215/spark_benchmark/tpcds/orc_1"
+  val INPUT_DIR_TPCDS = "/mnt/glusterfs/users/hza214/orc_1"
   val OUTPUT_DIR_TPCDS = "/localhdd/hza215/spark_benchmark/tpcds/"
   qConfig.format = "orc"
   qConfig.tpch_queryNum_start = 1
@@ -345,9 +345,9 @@ object SQL2FPGA_Top {
     qConfig.tpcds_queryNum_end = 99
 
     qConfig.tpcds_queryNum_list = ListBuffer(
-      72
+      11,14,15,17,23,24,28,50,64,67
       //4, 9, 10, 11, 14, 23, 24, 28, 72, 75 , 77, 78, 80, 85, 88, 93, 94, 95
-      // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99
+     //  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99
     ) // 1, 2, 3, 5, 6, 7, 8, 9
 
     // Start Spark Session
